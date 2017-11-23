e58cda71ad71aea90851a34505c4dd67866fb82e
vjragavan <vijay@onepeloton.com>
2017-11-23 09:31:32 -0500

    added toggle all to toggle all todos

---
+++ todos/todos_v5.js
@@ -0,0 +1,72 @@
+// Code goes here
+
+var todoList = {
+
+    todos: [],
+
+    displayTodos: function () {
+        if (this.todos.length === 0)
+            console.log('Todo list is empty!');
+        else {
+            console.log('My Todos');
+            for (var i=0; i<this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log ('(x) ',this.todos[i].todoText)
+                } else {
+                    console.log('( ) ',this.todos[i].todoText);
+                }
+            }
+        }
+    },
+
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+
+    toggleCompleted: function(position) {
+        this.todos[position].completed = !this.todos[position].completed;
+    },
+
+
+    toggleAll: function() {
+        var completed = 0;
+        var totalTodos = this.todos.length;
+
+        //count completed
+        for (var i=0; i<totalTodos; i++)
+            if (this.todos[i].completed === true)
+                completed++;
+
+        //case 1 : if all are completed, mark them false
+        if (completed === totalTodos) {
+            for (var i=0; i<totalTodos; i++)
+                this.todos[i].completed = false;
+        }
+        else {
+            for (var i=0; i<totalTodos; i++)
+                this.todos[i].completed = true;
+        }
+
+        this.displayTodos();
+    }
+
+};
+
+todoList.addTodo('Test1');
+todoList.addTodo('Test2');
+todoList.addTodo('Test3');
+todoList.displayTodos();
+todoList.toggleCompleted(2);
+todoList.toggleAll();
+todoList.toggleAll();
+todoList.deleteTodo(1);
+todoList.displayTodos();
